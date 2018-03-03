651f0f802bd7d7872f67947cd6b967dbb4fcf7ba
Gerrit van Aaken <gerrit@whitespace.gmbh>
2018-03-03 12:19:57 +0100

    added dynamic ranking

--- src/components/Player.vue
+++ src/components/Player.vue
@@ -2,6 +2,7 @@
 	<article class="players__player player">
 
 		<h3 class="player__name">{{ player.name }}</h3>
+		<h2>Rank Difference: {{ rankDifference }}</h2>
 		<button @click="addRandomSong()">Add random song</button>
 		<ul>
 			<bet-song v-for="(song, index) in player.ranking" :song="song" :key="index" :index="index"></bet-song>
@@ -29,6 +30,21 @@ export default {
 		addRandomSong() {
 			// does not hot-reference the song. Unfortunately. Can we fix that?
 			this.player.ranking.push(this.songs[Math.floor(Math.random() * 26)]);
+		}
+	},
+	computed: {
+		rankDifference() {
+			var rankDiff = 0;
+			var v = this;
+			v.songs.forEach(function(song, i){
+				v.player.ranking.forEach(function(songGuessed, j) {
+					if (song.country === songGuessed.country) {
+						rankDiff += Math.abs(i-j);
+					}
+				});
+				
+			});
+			return rankDiff;
 		}
 	}
 }
